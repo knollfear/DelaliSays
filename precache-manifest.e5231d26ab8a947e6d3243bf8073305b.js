84237e762e198c3fcd73c53eaca4e9f8d366b93d
Michael Knoll <mknoll@fearless.tech>
2020-02-09 14:43:09 -0500

    Updates

---
+++ precache-manifest.e5231d26ab8a947e6d3243bf8073305b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0737261698e50e1a37c00aa47c6a6094",
+    "url": "/DelaliSays/index.html"
+  },
+  {
+    "revision": "523fa5563b4c75983118",
+    "url": "/DelaliSays/static/css/main.da8508bd.chunk.css"
+  },
+  {
+    "revision": "37f50fce80bd88f26810",
+    "url": "/DelaliSays/static/js/2.bf897b86.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/DelaliSays/static/js/2.bf897b86.chunk.js.LICENSE"
+  },
+  {
+    "revision": "523fa5563b4c75983118",
+    "url": "/DelaliSays/static/js/main.3fbf4604.chunk.js"
+  },
+  {
+    "revision": "78cd8fec94d0aa46ea74",
+    "url": "/DelaliSays/static/js/runtime-main.2cd7c18b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/DelaliSays/static/media/logo.5d5d9eef.svg"
+  }
+]);
